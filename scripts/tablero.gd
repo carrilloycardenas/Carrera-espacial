60e4d87c1557d34fe341638c754e1550c04f9b47
Daniel Cárdenas <105086706+daca129@users.noreply.github.com>
2023-03-06 11:41:22 -0700

    Dado and tablero

--- scripts/tablero.gd
+++ scripts/tablero.gd
@@ -4,13 +4,8 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var a = "Player1"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	get_node("Fondo-espacio-dado/Label").text = str(a)
