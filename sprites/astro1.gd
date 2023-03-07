122d1329090813b32030963be6df41922668416e
Daniel Cárdenas <105086706+daca129@users.noreply.github.com>
2023-03-07 13:20:05 -0700

    Correct pixel advance

--- sprites/astro1.gd
+++ sprites/astro1.gd
@@ -4,21 +4,21 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var currPos = [576,552]
+var currPos = [579,546]
 
 # Called when the node enters the scene tree for the first time.
 func _input(event):
 	if event.is_action_pressed("ui_right"):
-		currPos[0] += 50
+		currPos[0] += 48.5
 		#get_node("Sprite").look_at(self.position + Vector2(1,0))
 	elif event.is_action_pressed("ui_left"):
-		currPos[0] -= 50
+		currPos[0] -= 48.5
 		#get_node("Sprite").look_at(self.position + Vector2(-1,0))
 	elif event.is_action_pressed("ui_up"):
-		currPos[1] -= 50
+		currPos[1] -= 48.5
 		#get_node("Sprite").look_at(self.position + Vector2(0,-1))
 	elif event.is_action_pressed("ui_down"):
-		currPos[1] += 50
+		currPos[1] += 48.5
 		#get_node("Sprite").look_at(self.position + Vector2(0,1))
 		
 	self.position = Vector2(currPos[0], currPos[1])
