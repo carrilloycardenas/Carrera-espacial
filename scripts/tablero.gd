594e70a8d972cc14f8257fa3040bb6579881ff59
Daniel Cárdenas <105086706+daca129@users.noreply.github.com>
2023-03-06 20:11:21 -0700

    Various changes

--- scripts/tablero.gd
+++ scripts/tablero.gd
@@ -4,8 +4,9 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var a = "Player1"
+var a = "Daniel"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Fondo-espacio-dado/Label").text = str(a)
+	get_node("Fondo-espacio-dado/lbl-player1").text = str(a)
+	get_node("Fondo-espacio-dado/lbl-player2").visible = false
