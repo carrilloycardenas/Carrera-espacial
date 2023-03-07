b2bbba7aae24435e025380d81f94441f086af6c9
IngCarrillo <105088732+IngCarrillo@users.noreply.github.com>
2023-03-07 13:08:55 -0700

    Update astro1.gd

--- sprites/astro1.gd
+++ sprites/astro1.gd
@@ -4,22 +4,22 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var currPos = [0,0]
+var currPos = [576,552]
 
 # Called when the node enters the scene tree for the first time.
 func _input(event):
 	if event.is_action_pressed("ui_right"):
-		currPos[0] += 32
-		get_node("Sprite").look_at(self.position + Vector2(1,0))
+		currPos[0] += 50
+		#get_node("Sprite").look_at(self.position + Vector2(1,0))
 	elif event.is_action_pressed("ui_left"):
-		currPos[0] -= 32
-		get_node("Sprite").look_at(self.position + Vector2(-1,0))
+		currPos[0] -= 50
+		#get_node("Sprite").look_at(self.position + Vector2(-1,0))
 	elif event.is_action_pressed("ui_up"):
-		currPos[1] -= 32
-		get_node("Sprite").look_at(self.position + Vector2(0,-1))
+		currPos[1] -= 50
+		#get_node("Sprite").look_at(self.position + Vector2(0,-1))
 	elif event.is_action_pressed("ui_down"):
-		currPos[1] += 32
-		get_node("Sprite").look_at(self.position + Vector2(0,1))
+		currPos[1] += 50
+		#get_node("Sprite").look_at(self.position + Vector2(0,1))
 		
 	self.position = Vector2(currPos[0], currPos[1])
 
