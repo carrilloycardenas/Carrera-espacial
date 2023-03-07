8cdb396e57c5078c045736fd253d606ec26cac48
Daniel Cárdenas <105086706+daca129@users.noreply.github.com>
2023-03-06 20:51:30 -0700

    More changes

--- scripts/tablero.gd
+++ scripts/tablero.gd
@@ -5,8 +5,14 @@ extends Node
 # var a = 2
 # var b = "text"
 var a = "Daniel"
+var b = "carrillo"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("Fondo-espacio-dado/lbl-player1").text = str(a)
-	get_node("Fondo-espacio-dado/lbl-player2").visible = false
+	get_node("Fondo-espacio-dado/lbl-player2").text = str(b)
+	get_node("Fondo-espacio-dado/lbl-player1").visible = true
+	get_node("Fondo-espacio-dado/lbl-player2").visible = true
+	get_node("Fondo-espacio-dado/lbl-player3").visible = false
+	get_node("Fondo-espacio-dado/lbl-player4").visible = false
+	
