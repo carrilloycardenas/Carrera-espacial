1a16e7e3d8e079dede0da12b1fc1b093f68a589e
Luis Carrillo <2020030196@upsin.edu.mx>
2023-03-10 07:10:32 -0700

    zi

--- scripts/tablero.gd
+++ scripts/tablero.gd
@@ -4,24 +4,21 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var a = "Daniel"
-var b = "carrillo"
+var players = ["Daniel", "Carrillo", "Eli", "Elvira"]
+@onready var astro1 = get_node("astro1")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Fondo-espacio-dado/lbl-player1").text = str(a)
-	get_node("Fondo-espacio-dado/lbl-player2").text = str(b)
-	get_node("Fondo-espacio-dado/lbl-player1").visible = true
-	get_node("Fondo-espacio-dado/lbl-player2").visible = true
-	get_node("Fondo-espacio-dado/lbl-player3").visible = false
-	get_node("Fondo-espacio-dado/lbl-player4").visible = false
+	for n in range(players.size()):
+		get_node("Fondo-espacio-dado/lbl-player"+str(n+1)).text = str(players[n])
 	
-
-
+func _on_btntirar_pressed():
+	var res = randi_range(1,6)
+	print(res)
+	astro1.avanzar(res)
 
 func _on_CheckButton_pressed():
-	OS.window_fullscreen = !OS.window_fullscreen
-
+	get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN if (!((get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN) or (get_window().mode == Window.MODE_FULLSCREEN))) else Window.MODE_WINDOWED
 
 func _on_btnsalir_pressed():
 	get_tree().quit()
