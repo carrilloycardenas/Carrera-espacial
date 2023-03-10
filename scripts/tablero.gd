13487153ae2f50b9d780afb0940a473654936b5a
Luis Carrillo <2020030196@upsin.edu.mx>
2023-03-10 09:52:57 -0700

    i

--- scripts/tablero.gd
+++ scripts/tablero.gd
@@ -3,6 +3,7 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var rng = RandomNumberGenerator.new()
 var players = ["Daniel", "Carrillo", "Eli", "Elvira"]
 onready var astro1 = get_node("astro1")
 
@@ -12,10 +13,7 @@ func _ready():
 		get_node("Fondo-espacio-dado/lbl-player"+str(n+1)).text = str(players[n])
 	
 func _on_btntirar_pressed():
-	var rng = RandomNumberGenerator.new()
-	var res = rng.randi_range(1,6)
-	print(res)
-	astro1.avanzar(res)
+	astro1.avanzar(rng.randi_range(1,6))
 
 func _on_CheckButton_pressed():
 	OS.window_fullscreen = !OS.window_fullscreen
