b9a421f388f8879c2c3e7492263b1f9dde6b9b89
Luis Carrillo <2020030196@upsin.edu.mx>
2023-04-10 22:08:26 -0700

    agujeros negros y cohetes con rangos

--- sprites/astro1.gd
+++ sprites/astro1.gd
@@ -68,22 +68,38 @@ func avanzar(dado):
 		elif currPos[0] < 579 && currPos[1] == 109.5:
 			currPos[0] += 48.5
 	# Comprobar casilla de agujero o cohete
-	if currPos[0] == 385 && currPos[1] == 449:
-		currPos[0] = [336.5,546]
-	elif currPos[0] == 336.5 && currPos[1] == 352:
-		currPos = [191,497.5]
-	elif currPos[0] == 142.5 && currPos[1] == 206.5:
-		currPos = [191,303.5]
-	elif currPos[0] == 336.5 && currPos[1] == 109.5:
-		currPos = [433.5,255]
-	elif currPos[0] == 530.5 && currPos[1] == 497.5:
-		currPos = [336.5,255]
-	elif currPos[0] == 191 && currPos[1] == 400.5:
-		currPos = [239.5,303.5]
-	elif currPos[0] == 288 && currPos[1] == 255:
-		currPos = [579,109.5]
-	elif currPos[0] == 239.5 && currPos[1] == 206.5:
-		currPos = [142.5,109.5]
+	# Casilla 19
+	if currPos[0] > 973 && currPos[0] < 1065 && currPos[1] > 814 && currPos[1] < 904:
+		currPos[0]-=(48.5*4)
+		currPos[1]-=(48.5*5)
+	# Casilla 32
+	elif currPos[0] > 320 && currPos[0] < 414 && currPos[1] > 626 && currPos[1] < 718:
+		currPos[0]+=(48.5)
+		currPos[1]-=(48.5*2)
+	# Casilla 67
+	elif currPos[0] > 507 && currPos[0] < 600 && currPos[1] > 347 && currPos[1] < 440:
+		currPos[0]+=(48.5*6)
+		currPos[1]-=(48.5*3)
+	# Casilla 73
+	elif currPos[0] > 414 && currPos[0] < 507 && currPos[1] > 254 && currPos[1] < 347:
+		currPos[0]-=(48.5*2)
+		currPos[1]-=(48.5*2)
+	# Casilla 26
+	elif currPos[0] > 694 && currPos[0] < 786 && currPos[1] > 718 && currPos[1] < 811:
+		currPos[0]-=(48.5)
+		currPos[1]+=(48.5*2)
+	# Casilla 46
+	elif currPos[0] > 600 && currPos[0] < 692 && currPos[1] > 533 && currPos[1] < 626:
+		currPos[0]-=(48.5*3)
+		currPos[1]+=(48.5*3)
+	# Casilla 71
+	elif currPos[0] > 228 && currPos[0] < 321 && currPos[1] > 255 && currPos[1] < 346:
+		currPos[0]+=(48.5)
+		currPos[1]+=(48.5*2)
+	# Casilla 95
+	elif currPos[0] > 600 && currPos[0] < 693 && currPos[1] > 69 && currPos[1] < 162:
+		currPos[0]+=(48.5*2)
+		currPos[1]+=(48.5*3)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
