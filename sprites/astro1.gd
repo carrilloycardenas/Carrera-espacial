5f581b4b423ab75b4ca59a341cc90cf1c489602a
Luis Carrillo <2020030196@upsin.edu.mx>
2023-04-10 20:31:16 -0700

    Agujeros negros y cohetes

--- sprites/astro1.gd
+++ sprites/astro1.gd
@@ -26,6 +26,7 @@ func _input(event):
 	print(currPos)
 
 func avanzar(dado):
+	# Movimiento en el tablero
 	for i in range(dado):
 		yield(get_tree().create_timer(1.0), "timeout")
 		if currPos[0] > 142.5 && currPos[1] == 546:
@@ -66,6 +67,23 @@ func avanzar(dado):
 			currPos[1] -= 48.5
 		elif currPos[0] < 579 && currPos[1] == 109.5:
 			currPos[0] += 48.5
+	# Comprobar casilla de agujero o cohete
+	if currPos[0] == 385 && currPos[1] == 449:
+		currPos = [336.5,546]
+	elif currPos[0] == 336.5 && currPos[1] == 352:
+		currPos = [191,497.5]
+	elif currPos[0] == 142.5 && currPos[1] == 206.5:
+		currPos = [191,303.5]
+	elif currPos[0] == 336.5 && currPos[1] == 109.5:
+		currPos = [433.5,255]
+	elif currPos[0] == 530.5 && currPos[1] == 497.5:
+		currPos = [336.5,255]
+	elif currPos[0] == 191 && currPos[1] == 400.5:
+		currPos = [239.5,303.5]
+	elif currPos[0] == 288 && currPos[1] == 255:
+		currPos = [579,109.5]
+	elif currPos[0] == 239.5 && currPos[1] == 206.5:
+		currPos = [142.5,109.5]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
